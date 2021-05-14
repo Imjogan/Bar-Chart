7aa5e4238578a671b93e0005a4f8ef9ca962fd0f
Mjogan <wow.mjogan.official@yandex.ru>
2021-05-14 19:18:30 +0300

    added adaptive

--- src/components/BarChart/BarChart.js
+++ src/components/BarChart/BarChart.js
@@ -1,11 +1,11 @@
 import './BarChart.css';
 import Chart from './Chart/Chart';
 
-function BarChart({data}) {
+function BarChart({ data }) {
   return (
     <section className="bar-chart">
       {data.map((dataForChart, i) => (
-        <Chart key={i} dataForChart={dataForChart}  />
+        <Chart key={i} dataForChart={dataForChart} />
       ))}
     </section>
   );
--- src/components/BarChart/Chart/Chart.js
+++ src/components/BarChart/Chart/Chart.js
@@ -1,20 +1,21 @@
 import './Chart.css';
 
-function Chart({dataForChart}) {
-
+function Chart({ dataForChart }) {
   const styleOnChart = {
     height: `${dataForChart.value}px`,
     backgroundColor: `${dataForChart.color}`,
-  }
+  };
 
   const styleOnValue = {
     color: `${dataForChart.color}`,
-  }
+  };
 
-  console.log(styleOnChart)
+  console.log(styleOnChart);
   return (
     <section className="chart">
-      <h2 style={styleOnValue} className="chart__value">{dataForChart.value}</h2>
+      <h2 style={styleOnValue} className="chart__value">
+        {dataForChart.value}
+      </h2>
       <div style={styleOnChart} className="chart__column" />
     </section>
   );
